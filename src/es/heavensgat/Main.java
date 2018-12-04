9e7d4ba66e5067a5526f5979b8d2675b171fa7e1
LTHeaven <benediktbender@gmail.com>
2018-12-04 19:24:33 +0100

    some things i couldnt even fucking test cause intellij is being a mayor bitch

--- src/es/heavensgat/Main.java
+++ src/es/heavensgat/Main.java
@@ -1,14 +1,40 @@
 package es.heavensgat;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         findChecksum("test.txt");
         findChecksum("input.txt");
+        partTwo("test.txt");
+        partTwo("input.txt");
+    }
+
+    private static void partTwo(String fileName) {
+        Scanner scanner = new Scanner(Main.class.getResourceAsStream(fileName));
+        List<String> strings = new ArrayList<>();
+        while (scanner.hasNext()){
+            strings.add(scanner.next());
+        }
+        String solution = "";
+        for (int i = 0; i<strings.size(); i++){
+            for (int j = i+1; j < strings.size(); j++){
+                char[] string1 = strings.get(i).toCharArray();
+                char[] string2 = strings.get(j).toCharArray();
+                int count = 0;
+                for(int k = 0; k < string1.length; k++){
+                    if (string1[k] != string2[k]){
+                        count++;
+                    }
+                    if (count == 1){
+                        solution= strings.get(i);
+                        solution = solution.substring(0, k) + solution.substring(k+1);
+                    }
+                }
+            }
+        }
+        System.out.println("Solution for " + fileName + ": " + solution);
     }
 
     private static void findChecksum(String fileName) {
